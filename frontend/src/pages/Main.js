5a270ffae99bee68fb73d4faaad7f440de21cba1
João Paulo Ferreira de Souza <joao.ferreiradesouza@hotmail.com>
2019-08-09 22:30:53 -0300

    Finalização do front

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,93 +1,82 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
+import {Link} from 'react-router-dom';
 import logo from '../assets/logo.svg'
 import like from '../assets/like.svg'
 import dislike from '../assets/dislike.svg'
+import api from '../services/api'
 import './Main.css'
 
 export default function Main({match}){
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=>{
+        async function loadUsers(){
+            const response = await api.get('/devs',{
+                headers:{
+                    user: match.params.id,
+                }                   
+            })
+            setUsers(response.data)
+            console.log(response.data)
+        }
+
+        loadUsers();
+    },[match.params.id])
+
+    async function handleLike(id){
+        await api.post(`/devs/${id}/likes`,null,{
+            headers: {user: match.params.id},
+        })
+        setUsers(users.filter(user=>user._id !== id));
+    }
+
+    async function handleDislike(id){
+        await api.post(`/devs/${id}/dislikes`,null,{
+            headers: {user: match.params.id},
+        })
+        setUsers(users.filter(user=>user._id !== id));
+    }
+
     return (
 <div className="main-container">
+
+
+    <Link to="/">
     <img src={logo} alt="Tindev"/>
-    <ul>
-        <li>
-            <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt=""/>
-       
+    </Link>
+
+
+       {users.length >0 ? (<ul>
+    {users.map(user=>(
+    <li key={user._id }>
+        <img src={user.avatar} alt={user.name} />
         <footer>
-            <strong>Diego 3G</strong>
-            <p>Teste de perfil </p>
-        </footer>
+            <strong>{user.name}</strong>
+            <p>{user.user} - {user.bio} </p>
+        </footer>   
+        
+            <div className="buttons">
 
-        <div className="buttons">
-            <button type="button">
-                <img src={like} alt=""/>
+            <button type="button" onClick={()=>handleDislike(user._id)}>
+                <img src={dislike} alt="" />
+            </button>
+            
+            <button type="button" onClick={()=>handleLike(user._id)}>
+                <img src={like} alt="" />
             </button>
 
-            <button type="button">
-                <img src={dislike} alt=""/>
-            </button>
-        </div>    
-        </li> 
-
-        <li>
-            <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt=""/>
-       
-        <footer>
-            <strong>Diego 3G</strong>
-            <p>Teste de perfil </p>
-        </footer>
-
-        <div className="buttons">
-            <button type="button">
-                <img src={like} alt=""/>
-            </button>
-
-            <button type="button">
-                <img src={dislike} alt=""/>
-            </button>
-        </div>    
-        </li> 
-
-        <li>
-            <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt=""/>
-       
-        <footer>
-            <strong>Diego 3G</strong>
-            <p>Teste de perfil </p>
-        </footer>
-
-        <div className="buttons">
-            <button type="button">
-                <img src={like} alt=""/>
-            </button>
-
-            <button type="button">
-                <img src={dislike} alt=""/>
-            </button>
-        </div>    
-        </li> 
-
-        <li>
-            <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt=""/>
-       
-        <footer>
-            <strong>Diego 3G</strong>
-            <p>Teste de perfil </p>
-        </footer>
-
-        <div className="buttons">
-            <button type="button">
-                <img src={like} alt=""/>
-            </button>
-
-            <button type="button">
-                <img src={dislike} alt=""/>
-            </button>
-        </div>    
-        </li>  
-    </ul>
+        </div>
+    </li>
+    ))}
+</ul>        
 
 
+       ):(
+        <div className="empty">Acabou :(</div>
 
-</div>
-    );
-};
+       ) }
+    
+    </div>
+    )
+}
